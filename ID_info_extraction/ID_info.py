2a0327f392414a3cf732b6b8c4ce109a3b496124
Yash Oza <60883716+yashoza1203@users.noreply.github.com>
2024-02-08 12:45:54 +0530

    Update ID_info.py

--- ID_info_extraction/ID_info.py
+++ ID_info_extraction/ID_info.py
@@ -18,7 +18,7 @@ import helpers.tesseract as tesseract
 
 class ID_EXTRACT:
     def __init__(self):
-        self.custom_config = r'--oem 3 --psm 6'
+        self.custom_config = r'--psm 6'
         pytesseract.pytesseract.tesseract_cmd = tesseract.find_tesseract_binary()
         self.sift = cv2.SIFT_create()
         self.characters_to_remove = ['|', '\n', 'i','i\n','[',']']
