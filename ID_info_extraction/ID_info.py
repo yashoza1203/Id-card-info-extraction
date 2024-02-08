35e2e09e5ae37c770bbb6b3ecd5aeca0eb8bc418
Yash Oza <60883716+yashoza1203@users.noreply.github.com>
2024-02-08 10:45:12 +0530

    Update ID_info.py

--- ID_info_extraction/ID_info.py
+++ ID_info_extraction/ID_info.py
@@ -19,6 +19,7 @@ import shutil
 class ID_EXTRACT:
     def __init__(self,tesseract_cmd):
         # pytesseract.pytesseract.tesseract_cmd = r'/usr/bin/tesseract'
+        pytesseract.pytesseract.tesseract_cmd = r'C:/Program Files/Tesseract-OCR/tesseract.exe'
         self.sift = cv2.SIFT_create()
         self.characters_to_remove = ['|', '\n', 'i','i\n']
 
