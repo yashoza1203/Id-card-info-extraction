d1d1e29e180302ececc661fe46154918786672ae
Yash Oza <60883716+yashoza1203@users.noreply.github.com>
2024-02-11 12:29:21 +0530

    Update ID_info.py

--- ID_info_extraction/ID_info.py
+++ ID_info_extraction/ID_info.py
@@ -189,7 +189,7 @@ st.markdown("## By:- Dev, Yash and Rushank 🤘")
 file_name  = st.text_input('Enter the File Name where you want to store the details:', 'id_details.csv')
 st.write('The current file is', file_name)
 
-query_image = st.file_uploader('Choose an ID',type='.jpg')
+query_image = st.file_uploader('Choose an ID',type=['.jpg','.jpeg'])
 submit = st.button('Extract Information')
 # st.write(query_image.read())
 
